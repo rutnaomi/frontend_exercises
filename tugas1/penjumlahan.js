0eb04694aef5f210bbce9b95df70e9661e01e1b9
rutnaomi <rutnaomiestersitompul@gmail.com>
2024-09-09 20:39:29 +0700

    Tugas 1 ygy huhuu!!

---
+++ tugas1/penjumlahan.js
@@ -0,0 +1,12 @@
+const penjumlahan = (a, b) => {
+    console.log(a + b);
+};
+
+penjumlahan(10, 5);
+
+// atauuu.....
+// let a = 10;
+// let b = 5;
+// let c = a + b;
+
+// console.log(c); 
---
+++ tugas1/variabel.js
@@ -0,0 +1,7 @@
+let nama = "Rut Naomi";   
+let umur = 19;           
+let apakahMahasiswa = true; 
+
+console.log(nama);
+console.log(umur);
+console.log(apakahMahasiswa);
